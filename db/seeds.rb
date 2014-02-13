6f4858b9d91e45da8410a23d9f2ed32f30973f16
Joe G <wheeltirefamily@gmail.com>
2014-02-12 21:50:20 -0600

    added wheel pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,124 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Product.destroy_all
 
-Product.create(name: "Unicorn Meat", picture: "51sGATfX3NL.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "Bacon + Mayo", picture: "baconnaise.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "BatMan", picture: "batshelf.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "Bees", picture: "bees.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "BigFoot", picture: "bigfoot-sweater.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "BigFoot", picture: "bigfoot.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "Cthulhu", picture: "cthulhu.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "Fez", picture: "fez.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "Chair", picture: "grass-chair.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "Kiss", picture: "kiss.jpeg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "Lego", picture: "lego.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "Mustache", picture: "mustache.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "Pancakes", picture: "pancake-maker.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "Squirrel", picture: "squirrel.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "StarWars", picture: "starwars-shirt.jpeg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "Tank", picture: "tank.jpeg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "Tentacles", picture: "tentacles.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
-Product.create(name: "Uranium", picture: "uranium.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "Unicorn Meat", picture: "_IROC-5_BMF_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "Bacon + Mayo", picture: "_IROC-6_BMF_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BatMan", picture: "ARM_502_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "Bees", picture: "ARM_502_FL_BMF_ST_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "ARM_505_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "ARM_505_FL_BMF_ST_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "ARM_506_FL_BMF_ST_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "ARM_507_FL_BMF_ST_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "ARM_507_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "ARM_507_SLV_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "ARM_508_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "ARM_508_FL_BMF_ST_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "ARM_508_SLV_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_BONNANO_BMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_BONNANO_BMF_ST_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_BONNANO_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_BONNANO_SLV_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_BUGSY_BLK_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_BUGSY_BLK_SSL_2.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_BUGSY_BMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_BUGSY_BMF_ST_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_BUGSY_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_BUGSY_GNMTL_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_BUGSY_SLV_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_COLUMBO_SLV_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_DE_LA_VEGA_BMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_ESCOBAR_GNMTL_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_FERRIGNO_BMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_FERRIGNO_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_FERRIGNO_SLV_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_GENOVESE_BLK_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_GENOVESE_SLV_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_MONTANA_BLK_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_MONTANA_BMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_MONTANA_MATTE_BMF_ST_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_MONTANA_SLV_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "DONZ_MONTANA_SLV_SSL_2.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "EST_ARIES_BMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "EST_ARIES_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "EST_CAPELLA_BMF_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "EST_CAPELLA_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "EST_CRUX_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "EST_CRUX_BMF_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "EST_CURSA_BMF_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "EST_CURSA_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "EST_POLARIS_CHR_BLKINS_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "EST_SAGITTA_BMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "EST_SAGITTA_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "FOR_F-301_BLK_PL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "FOR_F-302_BLK_RT_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "FOR_F-303_BMF_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "FOR_F-304_BLK_PL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "FOR_F-305_BMF_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "FOR_F-305_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "FOR_F-306_BMF_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "FOR_F-306_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "FOR_F-307_BMF_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "FOR_F-307_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "FOR_F-308_BMF_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "FOR_F-309_BMF_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "FOR_F-310_BMF_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "LEXANI_LX-SIX_2.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "LEXANI_LX-SIX.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M4_BMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M4_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M6_BMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M6_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M6_WMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M10_BMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M10_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M11_BMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M11_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M11_WMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M13_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M16_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M20_BMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M20_CHR_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+Product.create(name: "BigFoot", picture: "MER_M20_WMF_SSL_.jpg", cost: 499, item: "ABC123X", description: "Awesome Sauce")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
